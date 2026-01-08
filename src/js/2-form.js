bd492fc533c2378dd1ab0a06a68a61abbc25e676
Егор Губчак <zzzlodei777@gmail.com>
2026-01-09 00:31:47 +0300

    3

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+function saveFormData() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  if (name in formData) {
+    formData[name] = value.trim();
+    saveFormData();
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  formData = { email: '', message: '' };
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+loadFormData();
